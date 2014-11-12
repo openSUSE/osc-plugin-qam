47e60a2331d52e03b89271b02e8807995804b975
Florian Bergmann <fbergmann@suse.de>
2014-11-12 11:21:00 +0100

    Initial setup file.
    Signed-off-by: Florian Bergmann <fbergmann@suse.de>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+package = 'oscqam'
+version = '0.1'
+
+setup(
+    name=package,
+    version=version,
+    description='Plugin for OSC to support the workflow for the QA ' +
+    'maintenance department when using the new request / review osc ' +
+    'abstractions.',
+    url='url',
+)
