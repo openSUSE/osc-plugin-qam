e7fc46b1e5e32e402af6d55b1b9a6300c99786d5
Ondřej Súkup <osukup@suse.com>
2022-07-08 09:11:18 +0200

    Remove memoize decorator in favor of std.lib lru_cache

--- oscqam/utils.py
+++ oscqam/utils.py
@@ -1,29 +1,7 @@
-from functools import wraps
 from itertools import groupby
 import ssl
 from urllib.error import HTTPError
 from urllib.request import urlopen
-
-
-# TODO: LRU cache ?
-def memoize(func):
-    """Memoizes a function to reduce repetitive calling costs.
-
-    To work the decorated function must have hashable arguments.
-    """
-    cache = {}
-
-    @wraps(func)
-    def wrapped(*args, **kwargs):
-        sorted_keys = sorted(kwargs.keys())
-        arguments = (frozenset(args), frozenset((k, kwargs[k]) for k in sorted_keys))
-        if arguments in cache:
-            return cache[arguments]
-        result = func(*args, **kwargs)
-        cache[arguments] = result
-        return result
-
-    return wrapped
 
 
 def https(url):
